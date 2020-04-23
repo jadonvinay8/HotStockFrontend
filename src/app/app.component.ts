2749d137653879ea10ab13c6aba1d90df20bdd08
Vinay Pratap Singh <jadonvinay8@gmail.com>
2020-04-23 20:37:27 +0530

    Stock Data Retrieval from backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {StockDataService} from './services/stock-data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front';
+
+  request = {
+    ticker: '',
+    period: ''
+  };
+
+  showData = false;
+  stockData = {};
+  keys = [];
+  dates = [];
+  constructor(private service: StockDataService) {}
+
+  // getData() {
+  //   this.service.getNums().subscribe(
+  //     data => {
+  //               data = JSON.parse(data);
+  //
+  //               for (const key in data) {
+  //                 if (data.hasOwnProperty(key)) {
+  //                   console.log(key);
+  //                   this.stockData[key] = data[key];
+  //                 }
+  //               }
+  //
+  //               this.keys = Object.keys(this.stockData);
+  //               this.dates = this.stockData[this.keys[0]];
+  //               this.keys = this.keys.filter(key => key !== 'Dates');
+  //               this.showData = true;
+  //
+  //     },
+  //     err => console.log('An error occurred: ' + err)
+  //   );
+  // }
+
+  getStockData() {
+    this.service.getStockData(this.request).subscribe(
+      data => {
+        data = JSON.parse(JSON.stringify(data));
+
+        for (const key in data) {
+          if (data.hasOwnProperty(key)) {
+            this.stockData[key] = data[key];
+          }
+        }
+
+        this.keys = Object.keys(this.stockData);
+        this.dates = this.stockData[this.keys[0]];
+        this.keys = this.keys.filter(key => key !== 'Dates');
+        this.showData = true;
+
+      },
+      err => console.log('An error occurred: ' + err)
+    );
+  }
+
+
 }
